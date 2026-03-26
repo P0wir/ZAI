59949bafcb54d84beafc68bef5f3006d000f9873
Mateusz Powirski <92019610+P0wir@users.noreply.github.com>
2026-03-26 09:11:38 +0100

    Add files via upload

---
+++ PythonProject1/biblioteka_filmow/movies/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView, DetailView
+from .models import Movie
+
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = "movies/movie_list.html"
+    context_object_name = "movies"
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = "movies/movie_detail.html"
+    context_object_name = "movie"
